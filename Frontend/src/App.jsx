12de5cab10324455a7f56ddb1eea29f44d439e0b
Saba <sabaazeem025@gmail.com>
2023-12-03 09:28:45 +0500

    create and run the server

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import {BrowserRouter,Route,Router,Routes} from 'react-router-dom';
+
+//Import pages
 import Home from './pages/Home';
 import Signin from './pages/Signin';
 import Signup from './pages/Signup';
 import About from './pages/About';
 import Profile from './pages/Profile';
+
+// Import Components 
 import Header from './components/Header';
 
 function App() {
--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,15 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaSearch } from "react-icons/fa";
 
 function Header() {
   return (
-    <ul>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/about'>About</Link></li>
-        <li><Link to='/profile'>Profile</Link></li>
-        <li><Link to='/sign-in'>Sign In</Link></li>
-        <li><Link to='/sign-up'>Sign Up</Link></li>
-    </ul>
+    <header className='bg-slate-300 shadow-md'>
+        <div className='flex justify-between items-center mx-auto max-w-6xl p-3'>
+            <Link to='/'>
+                <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+                    <span className='text-slate-600'>Saba</span>
+                    <span className='text-slate-900'>Estate</span>
+                </h1>
+            </Link>
+            <form className='bg-slate-100 p-2 rounded-md flex items-center'>
+                <input type="text" placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
+                <FaSearch className='text-slate-900'/>
+            </form>
+            <nav>
+                <ul className='flex gap-3'>
+                    <Link to='/'>
+                        <li className='text-slate-900 font-semibold hover:underline cursor-pointer hidden sm:inline'>Home</li>
+                    </Link>
+                    <Link to='/about'>
+                        <li className='text-slate-900 font-semibold hover:underline cursor-pointer hidden sm:inline'>About</li>
+                    </Link>
+                    <Link to='/sign-in'>
+                        <li className='text-slate-900 font-semibold hover:underline cursor-pointer'>Sign In</li>
+                    </Link>
+                </ul>
+            </nav>
+        </div>
+    </header>
   )
 }
 
