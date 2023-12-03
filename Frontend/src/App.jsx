606e43a9c5ea3932e6aee5c3a74892d1a5042c74
Saba <sabaazeem025@gmail.com>
2023-12-03 08:02:28 +0500

    create pages and routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React from 'react';
+import {BrowserRouter,Route,Router,Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className='text-red-500'>App</div>   
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/sign-in' element={<Signin />} />
+          <Route path='/sign-up' element={<Signup />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/profile' element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+      
+    </>
   )
 }
 
---
+++ Frontend/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/about'>About</Link></li>
+        <li><Link to='/profile'>Profile</Link></li>
+        <li><Link to='/sign-in'>Sign In</Link></li>
+        <li><Link to='/sign-up'>Sign Up</Link></li>
+    </ul>
+  )
+}
+
+export default Header
