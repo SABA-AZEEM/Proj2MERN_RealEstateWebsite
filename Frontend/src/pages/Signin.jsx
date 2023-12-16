6b0053920f10faf4ca18fce3a26e98f4b7866b09
Saba <sabaazeem025@gmail.com>
2023-12-17 00:26:19 +0500

    add sign in page functionality.

--- Frontend/src/pages/Signin.jsx
+++ Frontend/src/pages/Signin.jsx
@@ -1,7 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {Link,useNavigate} from 'react-router-dom';
 
 export default function Signin() {
+
+  const [formData, setFormData] = useState({});
+  const [error,setError] = useState(null);
+  const [loading,setLoading]=useState(false);
+  const navigate= useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]:e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e)=>{
+    try{
+      e.preventDefault();
+      setLoading(true);
+      const res = await fetch('http://localhost:3000/api/auth/signin',{
+        method:"POST",
+        headers: {
+          'Content-Type':'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if(data.success===false){
+        setError(data.message);
+        setLoading(false);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate('/')
+    }catch(error){
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+
   return (
-    <div>Signin</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='my-7 text-3xl font-semibold text-center'>Sign In</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input type="email" placeholder='Email' className='border rounded-lg p-3' id='email' onChange={handleChange}/>
+        <input type="password" placeholder='Password' className='border rounded-lg p-3' id='password' onChange={handleChange}/>
+        <button disabled={loading} className='rounded-lg p-3 bg-slate-700 text-white uppercase hover:opacity-95 disabled:opacity-80'>
+          {loading? 'Loading...' : 'sign in'}
+        </button>
+      </form>
+      <div className='flex mt-5 gap-2'>
+        <p>Dont have an Account?</p>
+        <Link to={'/sign-up'}>
+          <span className='text-blue-700'>Sign up</span>
+        </Link>
+      </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
+    </div>
   )
 }
--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,9 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
+import jwt from "jsonwebtoken";
+import {errorHandler} from '../utils/error.js';
 
+//sign up functionality
 export const signup = async (req,res,next)=>{
     const {username,email,password} = req.body;
     const hashedPassword = bcryptjs.hashSync(password,10);
@@ -13,6 +16,25 @@ export const signup = async (req,res,next)=>{
         await newUser.save();
         res.status(201).json('User Created Successfully!'); 
     } catch (error) {
+        next(error);
+    }
+}
+
+//sign in functionality
+export const signin = async (req,res,next) =>{
+    const {email,password} = req.body;
+    try{
+        const validUser = await User.findOne({email});
+        if(!validUser) return next(errorHandler(404,'User not found!'));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword) return next(errorHandler(401,'Wrong Credentials!'));
+        const token = jwt.sign({id: validUser._id},process.env.JWT_SECRET);
+        const { password:pass, ...rest} = validUser._doc;
+        res
+        .cookie('access_token',token,{httpOnle:true})
+        .status(200)
+        .json(rest)
+    }catch(error){
         next(error);
     }
 }
