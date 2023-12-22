6c9f693d865dbfd1096b0ef12a35b62ac03b20b4
Saba <sabaazeem025@gmail.com>
2023-12-23 00:57:45 +0500

    complete create listing page UI

--- Frontend/src/pages/CreateListing.jsx
+++ Frontend/src/pages/CreateListing.jsx
@@ -6,7 +6,9 @@ const CreateListing = () => {
         <h1 className='text-3xl font-semibold text-center my-7'>
             Create a Listing
         </h1>
-        <form className='flex flex-col sm:flex-row'>
+        <form className='flex flex-col sm:flex-row gap-4'>
+
+            {/* div for left side of page */}
             <div className='flex flex-col gap-4 flex-1'>
                 <input 
                     type="text"
@@ -31,7 +33,137 @@ const CreateListing = () => {
                     id='address'
                     required
                 />
+
+                {/* sub-div for check boxes */}
+                <div className='flex gap-6 flex-wrap'>
+                    <div className='flex gap-2'>
+                        <input
+                         type="checkbox" 
+                         id='sale' 
+                         className='w-5' 
+                        />
+                        <span>Sell</span>
+                    </div>
+                    <div className='flex gap-2'>
+                        <input
+                         type="checkbox" 
+                         id='rent' 
+                         className='w-5' 
+                        />
+                        <span>Rent</span>
+                    </div>
+                    <div className='flex gap-2'>
+                        <input
+                         type="checkbox" 
+                         id='parking' 
+                         className='w-5' 
+                        />
+                        <span>Parking Spot</span>
+                    </div>
+                    <div className='flex gap-2'>
+                        <input
+                         type="checkbox" 
+                         id='furnished' 
+                         className='w-5' 
+                        />
+                        <span>Furnished</span>
+                    </div>
+                    <div className='flex gap-2'>
+                        <input
+                         type="checkbox" 
+                         id='offer' 
+                         className='w-5' 
+                        />
+                        <span>Offer</span>
+                    </div>                    
+                </div>
+
+                {/* sub-div for no of items */}
+                <div className='flex flex-wrap gap-6'>
+                    <div className='flex items-center gap-2'>
+                        <input 
+                            type="number" 
+                            id='bedrooms'
+                            min='1'
+                            max='10'
+                            required
+                            className='p-2 border border-gray-300 rounded-lg'
+                        />
+                        <p>Beds</p>
+                    </div>
+                    <div className='flex items-center gap-2'>
+                        <input 
+                            type="number" 
+                            id='bathrooms'
+                            min='1'
+                            max='10'
+                            required
+                            className='p-2 border border-gray-300 rounded-lg'
+                        />
+                        <p>Baths</p>
+                    </div>
+                    <div className='flex items-center gap-2'>
+                        <input 
+                            type="number" 
+                            id='regularPrice'
+                            min='50'
+                            max='100000000'
+                            required
+                            className='p-3 border border-gray-300 rounded-lg'
+                        />
+                        <div  className='flex flex-col items-center'> 
+                            <p>Regular Price</p>
+                            <span className='text-xs'>($ / month)</span>
+                        </div>
+                    </div>
+                    <div className='flex items-center gap-2'>
+                        <input 
+                            type="number" 
+                            id='discountPrice'
+                            min='0'
+                            max='100000000'
+                            required
+                            className='p-3 border border-gray-300 rounded-lg'
+                        />
+                        <div  className='flex flex-col items-center'>
+                            <p>Discounted Price</p>
+                            <span className='text-xs'>($ / month)</span>
+                        </div>
+                    </div>
+                </div>
+
             </div>
+
+            {/* div for right side of page */}
+            <div className='flex flex-col flex-1 gap-4'>
+                <p className='font-semibold'>
+                    Images:
+                    <span className='font-normal text-gray-600 ml-2'>
+                        The first image will be the cover (max 6)
+                    </span>
+                </p>
+                <div className='flex gap-4'>
+                    <input 
+                        type="file" 
+                        id='images'
+                        accept='image/*'
+                        multiple
+                        className='p-3 border border-gray-300 rounded w-full'
+                    />
+                    <button
+                        type='button'
+                        className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80'
+                    >
+                        Upload
+                    </button>
+                </div>
+
+                <button className='p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+                    Create Listing
+                </button>
+
+            </div>
+
         </form>
     </main>
   )
